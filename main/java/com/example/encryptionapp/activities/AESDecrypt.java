1b281764b7d7b4e3f0b1cbcc50f01caf5a12dced
allo97 <slodczyk97@gmail.com>
2022-08-06 02:05:40 +0200

    removed unused import

--- main/java/com/example/encryptionapp/activities/AESDecrypt.java
+++ main/java/com/example/encryptionapp/activities/AESDecrypt.java
@@ -5,7 +5,6 @@ import static com.example.encryptionapp.services.DataService.AES_MODES;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
